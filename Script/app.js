57e703b2bb787340a9ba8258f78a9e39a011acbd
ledei <holybuzz@live.se>
2022-12-15 12:36:24 +0100

    skapar korten och hämtar info från meny sedan lägger ut de i html

--- Script/app.js
+++ Script/app.js
@@ -8,13 +8,14 @@ function createCard(meny) {
   const cardSubPara = document.createElement("p");
 
   card.innerHTML = `<img class ="card-img" src="${meny.imgSrc}" alt="${meny.alt}" width = "100">`;
-  card.appendChild(cardHeader);
-  card.appendChild(cardPara);
-  card.appendChild(cardSubPara);
 
   cardHeader.textContent = meny.title;
   cardPara.textContent = meny.para;
   cardSubPara.textContent = meny.subCat;
+
+  card.appendChild(cardHeader);
+  card.appendChild(cardPara);
+  card.appendChild(cardSubPara);
 
   card.className = "menu-card";
 
@@ -25,12 +26,10 @@ function createCard(meny) {
 function createDOMCard(container) {
   let test = meny.length;
   for (let i = 0; i < test; i++) {
-    let cards = createCard(i);
+    let cards = createCard(meny[i]);
 
     container.append(cards);
   }
 }
 
 createDOMCard(mainContent);
-
-console.log(meny[3].title);
