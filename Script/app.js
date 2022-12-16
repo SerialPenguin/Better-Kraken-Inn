d2ccecbb38a9ad9d7c43c46785278aaf04a89830
SerialPenguin <larsson.alexander@live.se>
2022-12-16 14:19:46 +0100

    Search menu work-in-progress

--- Script/app.js
+++ Script/app.js
@@ -57,3 +57,17 @@ Array.prototype.forEach.call(flags, function(e){
   }); 
 });
 
+function search_product() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('menu-card');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
