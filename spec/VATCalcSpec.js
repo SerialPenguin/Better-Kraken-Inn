2b7146f7e3a3cd905e415908f057c65d140f7139
Mikael Forstén <m4jken@gmail.com>
2022-12-20 14:20:18 +0100

    added new function in majk.js and a jasmine test testing it

---
+++ spec/VATCalcSpec.js
@@ -0,0 +1,9 @@
+describe('calculate VAT', () => {
+  it('should calculate how much of a price is VAT', () => {
+    let price = 112;
+
+    let result = calcVAT(price);
+
+    expect(result).toEqual(12);
+  });
+});
